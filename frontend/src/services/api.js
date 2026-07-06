13f2f8ba0f269e40936b98dc9ca7d8be505f6f7f
Kishore <kishore45k007@gmail.com>
2026-07-06 12:56:42 +0530

    env

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:3000/api'
+    baseURL: 'https://ems-g7y1.onrender.com/api'
 });
 
 API.interceptors.request.use((req) => {
